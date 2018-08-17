47789472170504ec4f07427bebc98697dd56518b
Ronan Lina <connivinggod7@gmail.com>
2018-08-17 11:14:25 +0800

    added user input for path. fixed errors with hidden dirs.

--- ReadMe.md
+++ ReadMe.md
@@ -1,4 +1,3 @@
 **NOTES**
 
-The program might encounter errors when hidden directories (such as .git folders)
-are present in the current path.
+* **FIXED**. The program might encounter errors when hidden directories (such as .git folders) are present in the current path.
--- conversion.py
+++ conversion.py
@@ -16,38 +16,38 @@ current_dir = str(os.getcwd()) + '\\'
 
 files = os.listdir(current_dir)
 
+save_path = input('folder name : ')
+
 for f in files:
+    if not str(f).startswith('.'):
+        if not str(os.path.splitext(f)[1]) == '.py':
 
-    if not str(os.path.splitext(f)[1]) == '.py':
+            converted_name = '\\' + str(os.path.splitext(f)[0]) + '.jpg'
 
-        converted_name = '\\' + str(os.path.splitext(f)[0]) + '.jpg'
+            im = Image.open(current_dir + f).convert('RGB')
+            
+            im.thumbnail(size, Image.ANTIALIAS)
 
-        im = Image.open(current_dir + f).convert('RGB')
+            orig_img_width = im.width
+            orig_img_height = im.height
+
+            conv_img = Image.new('RGB', size, canvas_color)
+
+            # center alignment
+            x1 = int(math.floor((width - im.width) / 2))
+            y1 = int(math.floor((height - im.height) / 2))
+
+            #pasting to canvas
+            conv_img.paste(im,(x1, y1, x1 + orig_img_width, y1 + orig_img_height))
+
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+
+            conv_img.save(save_path + converted_name ,'JPEG',quality = 95)
         
-        im.thumbnail(size, Image.ANTIALIAS)
-
-        orig_img_width = im.width
-        orig_img_height = im.height
-
-        conv_img = Image.new('RGB', size, canvas_color)
-
-        # center alignment
-        x1 = int(math.floor((width - im.width) / 2))
-        y1 = int(math.floor((height - im.height) / 2))
-
-        #pasting to canvas
-        conv_img.paste(im,(x1, y1, x1 + orig_img_width, y1 + orig_img_height))
-
-        save_path = 'CDO PREMIUM NEW PRODUCTS'
-
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-
-        conv_img.save(save_path + converted_name ,'JPEG',quality = 95)
-    
-    else:
-        pass
-        break
+        else:
+            pass
+            break
 
 #destroy original files
 for f in files:
